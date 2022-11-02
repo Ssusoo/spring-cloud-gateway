32763eb0689fd7660ba614b7ba9c960ec6f1f3b8
김수환 <zzanggoon8@hiconsy.com>
2022-11-02 12:24:07 +0900

    lombok dependency gradle Add

---
+++ src/main/java/me/ssu/springcloudgateway/config/CustomFilter.java
@@ -0,0 +1,9 @@
+package me.ssu.springcloudgateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomFilter {
+}
